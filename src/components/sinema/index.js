991b1c720b10ac5623995949dc1a40dac1180469
hosseiny136 <hosseiny136@gmail.com>
2020-04-12 11:48:19 +0430

    edit index

--- src/components/sinema/index.js
+++ src/components/sinema/index.js
@@ -4,28 +4,28 @@ import myjson from './myhome';
 import bootstrap from 'reactstrap'
 const Sinemacard = props => {
   const data = myjson.data[7].items[0];
-  const { title, image, showtime, subtitle, price, location } = data;
-  console.log(myjson);
-  console.log(data);
+  const { title, image, subtitle, } = data;
+  // console.log(myjson);
+  // console.log(data);
   return (
     <div className="consert_card">
-    <div className="consert_card-image rel">
-      <div className="consert_card_unhover">
-        <img src={image.size.original} alt="" />
-      </div>
-      <div className="consert_card_hover">
-        <div className="consert_card_hover-content">
-          <ul>
-           <li className="consert_card_hover-viewticket"><a href="#">مشاهده و خرید بلیط</a></li>
-          </ul>
+      <div className="consert_card-image rel">
+        <div className="consert_card_unhover">
+          <img src={image.size.original} alt="" />
+        </div>
+        <div className="consert_card_hover">
+          <div className="consert_card_hover-content">
+            <ul>
+              <li className="consert_card_hover-viewticket"><a href="#">مشاهده و خرید بلیط</a></li>
+            </ul>
+          </div>
         </div>
       </div>
+      <div className="consert_card_footer">
+        <h3 className="consert_card-title text-cont">{title}</h3>
+        <span className="consert_card-desc text-cont">{subtitle}</span>
+      </div>
     </div>
-    <div className="consert_card_footer">
-      <h3 className="consert_card-title text-cont">{title}</h3>
-      <span className="consert_card-desc text-cont">{subtitle}</span>
-    </div>
-  </div>
   );
 }
 export default Sinemacard;
